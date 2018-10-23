b2896848f749adaa203dd8a3670dd040fd3d9042
hurikake06 <hurikake.oracle@gmail.com>
2018-10-23 06:15:16 +0000

    home画面追加

--- app/controllers/cg/cg_app_controller.rb
+++ app/controllers/cg/cg_app_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class Cg::CgAppController < CgLayoutsController
-  def index; end
+  def index
+    if session[:login_state] == 'OK'
+      @user = Cg::User.find(session[:user_id])
+      render :home
+    end
+  end
 end
