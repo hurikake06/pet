0456253c69b537edbdef339426b7eb739ce3e717
negi <sigma_sura@outlook.jp>
2018-10-26 08:17:39 +0000

    パスの修正 #18

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  namespace :CuteGift do
-    root to:'/cg/cg_app#index'
-    get 'index' => '/cg/cg_app#index'
-    get 'login' => '/cg/users#login'
-    post 'login/:user_mode' => '/cg/users#pass_check'
-    get 'logout' => '/sessions#destroy'
-    get 'ChangeMode/:user_mode' => '/sessions#mode_change'
+  scope :CuteGift do
+    root to:'cg/cg_app#index'
+    get 'index' => 'cg/cg_app#index'
+    get 'login' => 'cg/users#login'
+    post 'login/:user_mode' => 'cg/users#pass_check', as: 'login_request'
+    get 'logout' => 'sessions#destroy'
+    get 'ChangeMode/:user_mode' => 'sessions#mode_change', as: 'change_mode'
 
     namespace :user do
       get 'new' => '/cg/users#new'
@@ -16,15 +16,16 @@ Rails.application.routes.draw do
 
       namespace :pet do
         get 'new' => '/cg/pets#new'
-        post 'new' => '/cg/pets#new'
+        post 'new' => '/cg/pets#new', as: 'new_request'
         get ':petname' => '/cg/pets#mypage'
         get ':petname/edit' => '/cg/pets#edit'
+        post ':petname/edit' => '/cg/pets#update', as: 'edit_request'
       end
     end
 
     namespace :show do
       get ':username', to: '/cg/users#show'
-      get ':username/pet/:petname', to: '/cg/pets#show'
+      get ':username/pet/:petname', to: 'cg/pets#show', as: 'pet'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
