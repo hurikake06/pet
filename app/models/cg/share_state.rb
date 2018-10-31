14e61bdac3cbad2f4822a6d2d8612ee332c4be3b
hurikake06 <hurikake.oracle@gmail.com>
2018-10-31 02:28:07 +0000

    share_stateモデル削除

--- app/models/cg/share_state.rb
+++
@@ -1,3 +0,0 @@
-class Cg::ShareState < ApplicationRecord
-  has_many :cg_shares, foreign_key: :share_states_id, primary_key: :id, class_name: 'Cg::Share'
-end
--- test/models/cg/share_state_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Cg::ShareStateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
