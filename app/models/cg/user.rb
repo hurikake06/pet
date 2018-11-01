53d7cc0d1a0fee363aac719c6473b3dd312cd5db
negi <sigma_sura@outlook.jp>
2018-11-01 03:09:10 +0000

    userのusername,password,emailバリデーション

--- app/models/cg/user.rb
+++ app/models/cg/user.rb
@@ -6,9 +6,24 @@ class Cg::User < ApplicationRecord
   has_many :cg_shares, class_name: 'Cg::Share'
   has_one :cg_user_detail, class_name: 'Cg::UserDetail'
 
-  validates :password, length: { minimum: 1}
-  validates :username, length: { in: 1..15, message: "15文字以内" }
-  validates :username, format: { with: /\A[a-zA1-9\_]+\z/, message: "英数字又は_(アンダースコア)のみ" }
+  validates :email, format: {
+    with: %r{\A([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-]+)*\z},
+    message: '正しいメールアドレスを入力してください'
+  }
+
+  validates :username,
+    length: { in: 1..15, message: '15文字以内' },
+    format: {
+      with: /\A[a-zA1-9\_]+\z/,
+      message: '英数字又は_(アンダースコア)のみ入力してください'
+    }
+
+  validates :password,
+    length: { in: 4..32, message: '4文字以上32文字以内で入力してください' },
+    format: {
+      with: %r{\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)(?=.*?[!-~&&[^a-zA-Z\d]])[!-~]+\z},
+      message: '半角英小文字/大文字/数字/記号を1文字以上用いてください'
+    }
 
   def pets
     Cg::Pet.where(cg_pets: { users_id: id })
