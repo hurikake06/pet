b7e50e789c024e6b533f4e00af60900cb0794f7b
hurikake06 <hurikake.oracle@gmail.com>
2018-11-05 07:18:43 +0000

    pet詳細モデル追加

---
+++ app/models/cg/pet_detail.rb
@@ -0,0 +1,4 @@
+class Cg::PetDetail < ApplicationRecord
+  belongs_to :cg_pet, foreign_key: :pets_id, primary_key: :id, class_name: 'Cg::Pet'
+  belongs_to :cg_info, foreign_key: :medical_info, primary_key: :id, class_name: 'Cg::Info'
+end
