9466512cb255923ad3e5b209985447f6e25c2a46
Honda <37363079+hurikake06@users.noreply.github.com>
2018-11-12 14:06:46 +0900

    シェア周りのコントローラーとビュー作成 (#59)
    * シェア周りのコントローラーとビュー作成
    
    コーディングスタイル修正
    不要なif文削除

--- app/models/cg/share.rb
+++ app/models/cg/share.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 class Cg::Share < ApplicationRecord
   belongs_to :cg_user, foreign_key: :users_id, primary_key: :id, class_name: 'Cg::User'
   belongs_to :cg_pet, foreign_key: :pets_id, primary_key: :id, class_name: 'Cg::Pet'
-  # belongs_to :cg_share_state, foreign_key: :share_states_id, primary_key: :id, class_name: 'Cg::ShareState'
+
+  def user
+    Cg::User.find(users_id)
+  end
+
+  def pet
+    Cg::Pet.find(pets_id)
+  end
+
+  def info
+    Cg::Info.find(share_info)
+  end
 end
--- app/models/cg/user_detail.rb
+++ app/models/cg/user_detail.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Cg::UserDetail < ApplicationRecord
   belongs_to :cg_user, foreign_key: :users_id, primary_key: :id, class_name: 'Cg::User'
   belongs_to :cg_info, foreign_key: :document_info, primary_key: :id, class_name: 'Cg::Info'
