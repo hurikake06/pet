e292d8b58e5fe3d8a0513b260ce66aa299517aaa
negi0109 <sigma_sura@outlook.jp>
2018-11-14 03:11:07 +0000

    ペット詳細モデルのバリデーション

--- app/models/cg/pet_detail.rb
+++ app/models/cg/pet_detail.rb
@@ -4,6 +4,23 @@ class Cg::PetDetail < ApplicationRecord
   belongs_to :cg_pet, foreign_key: :pets_id, primary_key: :id, class_name: 'Cg::Pet'
   belongs_to :cg_info, foreign_key: :medical_info, primary_key: :id, class_name: 'Cg::Info'
 
+  validates :variable_cost,
+            numericality: {
+              allow_blank: true,
+              greater_than_or_equal_to: 0
+            },
+            on: :update
+
+  validates :fixed_cost,
+            numericality: {
+              allow_blank: true,
+              greater_than_or_equal_to: 0
+            },
+            on: :update
+
+  validates :share_about,
+            length: { maximum: 100, message: '100文字以内' }
+
   def host_shares
     Cg::Share.where(pets_id: pets_id)
   end
