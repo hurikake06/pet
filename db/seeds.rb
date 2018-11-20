aace9e5ebfef6986103e9f5d64219eb23486bef9
negi0109 <sigma_sura@outlook.jp>
2018-11-20 01:02:59 +0000

    split seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,59 +8,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require './db/seeds/infoType.rb'
+require './db/seeds/info.rb'
 require './db/seeds/facility.rb'
-
-Cg::InfoType.create!(id: 1, value: 'share')
-Cg::InfoType.create!(id: 2, value: 'sex')
-Cg::InfoType.create!(id: 3, value: 'country')
-Cg::InfoType.create!(id: 4, value: 'share_user')
-Cg::InfoType.create!(id: 5, value: 'share_host')
-Cg::InfoType.create!(id: 6, value: 'share_pet')
-Cg::InfoType.create!(id: 7, value: 'document')
-Cg::InfoType.create!(id: 8, value: 'credit')
-Cg::InfoType.create!(id: 9, value: 'share_type')
-Cg::InfoType.create!(id: 10, value: 'medical')
-Cg::InfoType.create!(id: 11, value: 'pets_sex')
-Cg::InfoType.create!(id: 12, value: 'weekday')
-Cg::InfoType.create!(id: 100, value: 'system')
-
-Cg::Info.create!(id: 1, info_types_id: 1, value: '申請')
-Cg::Info.create!(id: 2, info_types_id: 1, value: '許可')
-Cg::Info.create!(id: 3, info_types_id: 1, value: '拒否')
-Cg::Info.create!(id: 4, info_types_id: 1, value: 'キャンセル')
-Cg::Info.create!(id: 5, info_types_id: 1, value: '支払い待ち')
-Cg::Info.create!(id: 6, info_types_id: 1, value: '終了')
-
-Cg::Info.create!(id: 7, info_types_id: 2, value: '男性')
-Cg::Info.create!(id: 8, info_types_id: 2, value: '女性')
-
-Cg::Info.create!(id: 9, info_types_id: 3, value: '日本')
-Cg::Info.create!(id: 10, info_types_id: 3, value: 'マルタ')
-Cg::Info.create!(id: 11, info_types_id: 3, value: 'アメリカ')
-
-Cg::Info.create!(id: 12, info_types_id: 4, value: '利用不可')
-Cg::Info.create!(id: 13, info_types_id: 4, value: '利用可能')
-Cg::Info.create!(id: 14, info_types_id: 5, value: '利用不可')
-Cg::Info.create!(id: 15, info_types_id: 5, value: '利用可能')
-Cg::Info.create!(id: 16, info_types_id: 6, value: '利用不可')
-Cg::Info.create!(id: 17, info_types_id: 6, value: '利用可能')
-
-Cg::Info.create!(id: 22, info_types_id: 9, value: '利用不可')
-Cg::Info.create!(id: 23, info_types_id: 9, value: '利用可能')
-
-Cg::Info.create!(id: 26, info_types_id: 11, value: 'オス')
-Cg::Info.create!(id: 27, info_types_id: 11, value: 'メス')
-
-Cg::Info.create!(id: 28, info_types_id: 12, value: '日')
-Cg::Info.create!(id: 29, info_types_id: 12, value: '月')
-Cg::Info.create!(id: 30, info_types_id: 12, value: '火')
-Cg::Info.create!(id: 31, info_types_id: 12, value: '水')
-Cg::Info.create!(id: 32, info_types_id: 12, value: '木')
-Cg::Info.create!(id: 33, info_types_id: 12, value: '金')
-Cg::Info.create!(id: 34, info_types_id: 12, value: '土')
-
-Cg::Info.create!(id: 100, info_types_id: 100, value: '未登録')
-Cg::Info.create!(id: 101, info_types_id: 100, value: '登録済み')
+require './db/seeds/type.rb'
 
 Cg::User.create!(
   id: 1,
@@ -113,18 +64,6 @@ Cg::User.create!(
   cg_user_detail_attributes: {
   }
 )
-
-Cg::Type.create!(id: 1, name: '犬', share_type_info: 23)
-
-Cg::Type.create!(id: 2, name: '猫', share_type_info: 23)
-
-Cg::Type.create!(id: 3, name: '鳥', share_type_info: 22)
-
-Cg::Type.create!(id: 4, name: '亀', share_type_info: 22)
-
-Cg::Type.create!(id: 5, name: '猿', share_type_info: 22)
-
-Cg::Type.create!(id: 6, name: '魚', share_type_info: 22)
 
 Cg::Pet.create!(
   id: 1,
---
+++ db/seeds/type.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Cg::Type.create!(id: 1, name: '犬', share_type_info: 23)
+Cg::Type.create!(id: 2, name: '猫', share_type_info: 23)
+Cg::Type.create!(id: 3, name: '鳥', share_type_info: 22)
+Cg::Type.create!(id: 4, name: '亀', share_type_info: 22)
+Cg::Type.create!(id: 5, name: '猿', share_type_info: 22)
+Cg::Type.create!(id: 6, name: '魚', share_type_info: 22)
