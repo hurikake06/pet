88b2036e7299410f26e85a08a4fefc12f29786fe
negi0109 <33025461+negi0109@users.noreply.github.com>
2018-11-21 10:04:58 +0900

    split seed (#70)
    * split seed
    
    * ファイル名修正
    
    * 改行コードの修正
    
    * fix 呼び出しパス

---
+++ db/seeds/info.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+Cg::Info.create!(id: 1, info_types_id: 1, value: '申請')
+Cg::Info.create!(id: 2, info_types_id: 1, value: '許可')
+Cg::Info.create!(id: 3, info_types_id: 1, value: '拒否')
+Cg::Info.create!(id: 4, info_types_id: 1, value: 'キャンセル')
+Cg::Info.create!(id: 5, info_types_id: 1, value: '支払い待ち')
+Cg::Info.create!(id: 6, info_types_id: 1, value: '終了')
+
+Cg::Info.create!(id: 7, info_types_id: 2, value: '男性')
+Cg::Info.create!(id: 8, info_types_id: 2, value: '女性')
+
+Cg::Info.create!(id: 9, info_types_id: 3, value: '日本')
+Cg::Info.create!(id: 10, info_types_id: 3, value: 'マルタ')
+Cg::Info.create!(id: 11, info_types_id: 3, value: 'アメリカ')
+
+Cg::Info.create!(id: 12, info_types_id: 4, value: '利用不可')
+Cg::Info.create!(id: 13, info_types_id: 4, value: '利用可能')
+Cg::Info.create!(id: 14, info_types_id: 5, value: '利用不可')
+Cg::Info.create!(id: 15, info_types_id: 5, value: '利用可能')
+Cg::Info.create!(id: 16, info_types_id: 6, value: '利用不可')
+Cg::Info.create!(id: 17, info_types_id: 6, value: '利用可能')
+
+Cg::Info.create!(id: 22, info_types_id: 9, value: '利用不可')
+Cg::Info.create!(id: 23, info_types_id: 9, value: '利用可能')
+
+Cg::Info.create!(id: 26, info_types_id: 11, value: 'オス')
+Cg::Info.create!(id: 27, info_types_id: 11, value: 'メス')
+
+Cg::Info.create!(id: 28, info_types_id: 12, value: '日')
+Cg::Info.create!(id: 29, info_types_id: 12, value: '月')
+Cg::Info.create!(id: 30, info_types_id: 12, value: '火')
+Cg::Info.create!(id: 31, info_types_id: 12, value: '水')
+Cg::Info.create!(id: 32, info_types_id: 12, value: '木')
+Cg::Info.create!(id: 33, info_types_id: 12, value: '金')
+Cg::Info.create!(id: 34, info_types_id: 12, value: '土')
+
+Cg::Info.create!(id: 100, info_types_id: 100, value: '未登録')
+Cg::Info.create!(id: 101, info_types_id: 100, value: '登録済み')
