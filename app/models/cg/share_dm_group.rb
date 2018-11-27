1184d28c21503faddb81953201ae8cb5bfdfb8ea
negi0109 <sigma_sura@outlook.jp>
2018-11-27 03:23:13 +0000

    抽象関数user,hostメソッドの実装 #82

--- app/models/cg/share_dm_group.rb
+++ app/models/cg/share_dm_group.rb
@@ -2,4 +2,12 @@
 
 class Cg::ShareDmGroup < Cg::DmGroup
   belongs_to :share, foreign_key: :shares_id, primary_key: :id, class_name: 'Cg::Share'
+
+  def host
+    share.pet.user
+  end
+
+  def user
+    share.user
+  end
 end
--- app/models/cg/user_dm_group.rb
+++ app/models/cg/user_dm_group.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Cg::UserDmGroup < Cg::DmGroup
-    belongs_to :user, foreign_key: :users_id, primary_key: :id, class_name: 'Cg::User'
-    belongs_to :pet, foreign_key: :pets_id, primary_key: :id, class_name: 'Cg::Pet'
+    belongs_to :cg_user, foreign_key: :users_id, primary_key: :id, class_name: 'Cg::User'
+    belongs_to :cg_pet, foreign_key: :pets_id, primary_key: :id, class_name: 'Cg::Pet'
+
+    def host
+      cg_pet.cg_user
+    end
 end
