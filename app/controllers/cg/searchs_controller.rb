9954e57b37e376aeaf775355a619deab33f65aee
negi0109 <sigma_sura@outlook.jp>
2018-12-12 02:16:43 +0000

    名前でのキーワード検索

--- app/controllers/cg/searchs_controller.rb
+++ app/controllers/cg/searchs_controller.rb
@@ -2,7 +2,15 @@
 
 class Cg::SearchsController < Cg::LayoutsController
   def index
-    @pets = Cg::Pet.all
-    @users = Cg::User.all
+    if params[:keyword].present?
+      @keyword = params[:keyword]
+      @pets = Cg::Pet.where('name LIKE ?', "%#{@keyword}%")
+                     .or(Cg::Pet.where('petname LIKE ?', "%#{@keyword}%"))
+      @users = Cg::User.where('name LIKE ?', "%#{@keyword}%")
+                       .or(Cg::Pet.where('username LIKE ?', "%#{@keyword}%"))
+    else
+      @pets = Cg::Pet.all
+      @users = Cg::User.all
+    end
   end
 end
