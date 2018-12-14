095ab269c92da481129a4e7de8e62e37ef551db6
hurikake06 <hurikake.oracle@gmail.com>
2018-12-14 02:27:04 +0000

    レイアウト切り分け

--- app/controllers/cg/cg_app_controller.rb
+++ app/controllers/cg/cg_app_controller.rb
@@ -2,9 +2,15 @@
 
 class Cg::CgAppController < Cg::LayoutsController
   def index
-    return unless login_flag
+    if login_flag
+      home
+    else
+      render layout: 'cg_layouts_width_stuffing'
+    end
+  end
 
+  def home
     @user = session_user
-    render :home
+    render :home, layout: 'cg_layouts_nomal'
   end
 end
