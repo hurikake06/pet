e546ad7f12dc8092809e3d0a08d3c4d107e68d91
negi0109 <sigma_sura@outlook.jp>
2018-12-19 01:34:06 +0000

    検索時のみ結果が表示されるよう変更

--- app/controllers/cg/searchs_controller.rb
+++ app/controllers/cg/searchs_controller.rb
@@ -2,15 +2,14 @@
 
 class Cg::SearchsController < Cg::LayoutsController
   def index
+    @searched = false
     if params[:keyword].present?
+      @searched = true
       @keyword = params[:keyword]
       @pets = Cg::Pet.where('name LIKE ?', "%#{@keyword}%")
                      .or(Cg::Pet.where('petname LIKE ?', "%#{@keyword}%"))
       @users = Cg::User.where('name LIKE ?', "%#{@keyword}%")
                        .or(Cg::Pet.where('username LIKE ?', "%#{@keyword}%"))
-    else
-      @pets = Cg::Pet.all
-      @users = Cg::User.all
     end
   end
 end
