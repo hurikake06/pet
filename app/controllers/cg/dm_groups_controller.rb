b1e1634f813054048d4f5eccc360a85eef970cb1
negi0109 <sigma_sura@outlook.jp>
2018-12-28 09:21:09 +0000

    add リスト表示変数

--- app/controllers/cg/dm_groups_controller.rb
+++ app/controllers/cg/dm_groups_controller.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class Cg::DmGroupsController < Cg::LayoutsController
-  def list; end
+  def list
+    login_check
+    @user = session_user
+    host_share_dm_groups = Cg::ShareDmGroup.eager_load(:share)
+                                           .where(
+                                             cg_shares: {
+                                               pet_id: Cg::Pet.where(user_id: @user.id).select(:id)
+                                             }
+                                           )
+    user_share_dm_groups = Cg::ShareDmGroup.eager_load(:share)
+                                           .where(cg_shares: { user_id: @user.id })
+  end
 
   def show
     login_check
@@ -9,21 +20,22 @@ class Cg::DmGroupsController < Cg::LayoutsController
 
   def show_share
     login_check
-    dmGroup = Cg::ShareDmGroup.find_by(share_id: params[:share_id])
-    if dmGroup.nil?
-      dmGroup = Cg::ShareDmGroup.create!(share_id: params[:share_id])
+    dm_group = Cg::ShareDmGroup.find_by(share_id: params[:share_id])
+    if dm_group.nil?
+      dm_group = Cg::ShareDmGroup.create!(share_id: params[:share_id])
     end
-    redirect_to cg_dm_groups_path dmGroup.id
+    redirect_to cg_dm_groups_path dm_group.id
   end
 
   def show_user
     login_check
     pet = Cg::Pet.find(params[:pet_id])
     return if pet.user_id == session_user.id
-    dmGroup = Cg::UserDmGroup.find_by(pet_id: pet.id,user_id: session_user.id)
-    if dmGroup.nil?
-      dmGroup = Cg::UserDmGroup.create!(pet_id: pet.id,user_id: session_user.id)
+
+    dm_group = Cg::UserDmGroup.find_by(pet_id: pet.id, user_id: session_user.id)
+    if dm_group.nil?
+      dm_group = Cg::UserDmGroup.create!(pet_id: pet.id, user_id: session_user.id)
     end
-    redirect_to cg_dm_groups_path dmGroup.id
+    redirect_to cg_dm_groups_path dm_group.id
   end
 end
