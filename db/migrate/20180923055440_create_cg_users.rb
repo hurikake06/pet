0fde5eb862206ea597505d6c122e7f45226fd973
negi0109 <sigma_sura@outlook.jp>
2019-01-08 01:48:06 +0000

    アイコンの初期値変更

--- db/migrate/20180923055440_create_cg_users.rb
+++ db/migrate/20180923055440_create_cg_users.rb
@@ -7,7 +7,7 @@ class CreateCgUsers < ActiveRecord::Migration[5.2]
       t.string :username
       t.string :email
       t.string :password
-      t.string :icon, default: 'icon/person/b_12.png'
+      t.string :icon
       t.text :about
       t.bigint :share_user_info, default: 12
       t.bigint :share_host_info, default: 14
