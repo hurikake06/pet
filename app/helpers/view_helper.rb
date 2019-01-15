60a847872f83053d8e596753e2a6a00fabc13d32
negi0109 <sigma_sura@outlook.jp>
2019-01-15 03:18:05 +0000

    DM中リンク表示

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
-
 module ViewHelper
+  require 'uri'
+
   def print_form_error(errors, key)
     if errors.present? && errors[key].present?
       str = ''
@@ -10,4 +10,14 @@ module ViewHelper
       str
     end
   end
+
+  def text_url_to_link text
+    URI.extract(text, ['http','https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
+    return text
+  end
+
 end
